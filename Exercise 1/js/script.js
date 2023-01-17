f0014bed71bd2e7a6ca10db5112f19bb4075f82d
elishakaye <114690670+elishakaye@users.noreply.github.com>
2023-01-16 20:54:49 -0500

    Update script.js

--- Exercise 1/js/script.js
+++ Exercise 1/js/script.js
@@ -1,18 +1,21 @@
 /**
-Title of Project
-Author Name
+Exercise One - Pong game
+Elisha Rodil 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
 "use strict";
-let x;
-let y;
+let x = 320;
+let y = 180;
 
-let speedX;
-let speedY;
+let speedX = 5;
+let speedY = 2;
 
-let speed = 10;
+let r = 25;
+
+let score = 0;
+let playerScore;
 
 
 /**
@@ -36,13 +39,31 @@ function setup() {
 Description of draw()
 */
 function draw() {
-    background(0); //add a background color to the canvas = black
+    background('#ebecf0'); //add a background color to the canvas = black
 
-    noStroke();
+    //paddles + its functions
+    fill(0);
+    rect(50, mouseY, 20, 100);
+    rect(935, mouseY, 20, 100);
+
+    //the ball feature of the game
     //background(230, 230, 250); //add a color to the ball = lavender
-    ellipse(20, 20, 40, 40); //create the ball using ellipse
+    //ellipse(20, 20, 40, 40); //create the ball using ellipse
+    noStroke();
+    fill(0);
+    ellipse(x, y, r*2, r*2);
+    x += speedX;
+    y += speedY;
+    if (x > width - r || x < r) {
+      speedX = -speedX;
+    }
+    if (y > height - r || y < r) {
+      speedY = -speedY;
+    }
 
-    //paddles
-    rect(50, mouseY, 20, 100)
-
+    //scores
+    fill(0);
+    textSize(24);
+    text("Score: " + score, 20 , 40);
+    //text(playerScore, 140,40);
 }
