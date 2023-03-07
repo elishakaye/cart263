3d86a309d95f6d3767fc6d58e43f395eb8bbf084
elishakaye <114690670+elishakaye@users.noreply.github.com>
2023-03-07 16:39:08 -0500

    updated

--- ProjectTwo/js/script.js
+++ ProjectTwo/js/script.js
@@ -10,17 +10,44 @@ author, and this description to match your project!
 /**
 Description of preload
 */
-
+var words = [];
+var values = [];
+var data;
 
 function preload() {
- 
+    data = loadTable("top_youtube_2.csv", "csv", "header");
 }
 
 /**
 Description of setup
 */
 function setup() {
- 
+    createCanvas(400, 400);
+    noStroke();
+    colorMode(HSB, 360, 100, 100, 100);
+    background(0);
+    //print(data);
+    
+    for(var i = 0; i < data.getRowCount(); i++){
+      for(var j = 0; j < data.getColumnCount(); j++){
+        if(j == 0) words.push(data.getString(i, j));
+        if(j == 1) values.push(data.getNum(i, j));
+      }
+    }
+    //print(words);
+
+    for(var i = 0; i < 20; i++){
+        var w = words[i];
+        var n = views[i];
+        var y = height/2;
+        var s = map(n, 0, max(views), 0, 50);
+        var c = map(n, 0, max(views, 0, 360));
+
+        fill(c, 100, 100);
+        textSize(s);
+        text(w, 0, y);
+        translate(textWidth(w), 0);
+    }
 }
 
 /**
